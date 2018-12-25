0c111c746a1e64b6959fdc3d6c077ef29573f249
haodong5502 <liuhaodong5201314a@qq.com>
2018-12-25 11:12:43 +0800

    更改第一次

--- src/test/com/newer/test/test1.java
+++ src/test/com/newer/test/test1.java
@@ -1,10 +1,16 @@
 package com.newer.test;
 
-import org.junit.Test;
+
+import org.junit.jupiter.api.Test;
 
 public class test1 {
     @Test
     public void tes1(){
         System.out.println("黄泽西");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("刘浩东");
     }
 }
