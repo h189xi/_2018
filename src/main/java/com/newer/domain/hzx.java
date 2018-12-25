a04b70226baaef1006a80f69137766b5c1344dac
jessy <595119090@qq.com>
2018-12-25 10:52:50 +0800

    Initial commit

---
+++ src/main/java/com/newer/domain/hzx.java
@@ -0,0 +1,20 @@
+package com.newer.domain;
+
+public class hzx {
+    private String name;
+
+    public hzx() {
+    }
+
+    public hzx(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/test/com/newer/test/test1.java
@@ -0,0 +1,10 @@
+package com.newer.test;
+
+import org.junit.Test;
+
+public class test1 {
+    @Test
+    public void tes1(){
+        System.out.println("黄泽西");
+    }
+}
